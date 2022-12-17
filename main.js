0caa15cf5c7ace80c12e291237f388c876e72035
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2022-12-18 04:52:26 +0900

    実装中

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const tasksTableBodyElement = document.getElementById("tasks-table-body");
+const taskTitleInputElement = document.getElementById("task-title-input");
+const taskAddButtonElement = document.getElementById("task-add-button");
 
 const loadTasks = async () => {
   await fetch('/api/tasks')
   .then((res) => {
+    // OKではなかった場合
+    if (!res.ok) {
+      alert("エラーが発生しました");
+      return;
+    }
+
+    // レスポンスからデータを取得する
     res.json().then((responseData) => {
+      
+      // タスク一覧をテーブルの形に変換する
       responseData.tasks.forEach((task) => {
         const titleTdElement = document.createElement("td");
         titleTdElement.innerText = task.title;
@@ -19,13 +30,51 @@ const loadTasks = async () => {
       });
     });
   }).catch((e) => {
+    alert("エラーが発生しました");
+    console.error(JSON.stringify(e));
+  })
+}
+
+const registerTask = async () => {
+  // 入力値を取得する
+  const title = taskTitleInputElement.value;
+
+  // 送信するデータ
+  const requestBody = {
+    title: title,
+  }
+
+  // APIに送信する
+  await fetch('/api/tasks', {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+  })
+  .then((res) => {
+    // OKではなかった場合
+    if (!res.ok) {
+      alert("エラーが発生しました");
+      return;
+    }
+
+    // 再取得をする
+    loadTasks();
+
+    // テキストボックスの中身を消す
+    taskTitleInputElement.value = "";
+
+  }).catch((e) => {
+    alert("エラーが発生しました");
     console.error(JSON.stringify(e));
   })
 }
 
 const main = async () => {
+  // ボタンクリック時のイベントを追加
+  taskAddButtonElement.addEventListener('click', registerTask);
+
   // タスク一覧を取得する
   await loadTasks();
+
 }
 
 main();
