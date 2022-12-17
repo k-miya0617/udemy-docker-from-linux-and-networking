033dabc91fa5fecd4478edab98ea75ab21150f66
K.Miya <51694607+k-miya0617@users.noreply.github.com>
2022-12-18 04:17:59 +0900

    画面上にメッセージを表示するよう修正

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const main = async () => {
   // APIからデータを取得する
   await fetch('/api/hello')
   .then((value) => {
-    console.log(JSON.stringify(value));
+    const messageElement = document.getElementById("message");
+    messageElement.innerHTML = value;
   }).catch((e) => {
     console.error(JSON.stringify(e));
   })
