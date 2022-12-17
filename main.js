4d1e3ad3dfbce228b7bed2505b73143060d9f8eb
K.Miyazawa <k.miyazawa.0617@gmail.com>
2022-12-18 04:37:12 +0900

    implements

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+const tasksTableBodyElement = document.getElementById("tasks-table-body");
 
 const loadTasks = async () => {
   await fetch('/api/tasks')
   .then((res) => {
-    res.json().then((value) => {
-      const messageElement = document.getElementById("message");
-      messageElement.innerHTML = value.message;
+    res.json().then((responseData) => {
+      responseData.tasks.forEach((task) => {
+        const titleTdElement = document.createElement("td");
+        titleTdElement.innerText = task.title;
+
+        const createdAtTdElement = document.createElement("td");
+        createdAtTdElement.innerText = task.createdAt.toLocaleString();
+
+        const trElement = document.createElement("tr");
+        trElement.appendChild(titleTdElement);
+        trElement.appendChild(createdAtTdElement);
+
+        tasksTableBodyElement.appendChild(trElement);
+      });
     });
   }).catch((e) => {
     console.error(JSON.stringify(e));
