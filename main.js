e9fc067b2b2d5a646b38a949a9bfd358efe9fa98
K.Miyazawa <k.miyazawa.0617@gmail.com>
2022-12-18 04:14:50 +0900

    fix it

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const main = async () => {
   // APIからデータを取得する
   await fetch('/api/hello')
-  .then((value) => {
-    console.log(JSON.stringify(value));
+  .then((res) => {
+    res.json().then((value) => {
+      console.log(JSON.stringify(value));
+    });
   }).catch((e) => {
     console.error(JSON.stringify(e));
   })
